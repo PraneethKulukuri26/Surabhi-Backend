470c6dfb5592ad5624cc51855d32eaeebc20a859
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2024-12-31 21:21:36 +0530

    Installed Requered files and configured files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const dotenv=require("dotenv");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const compression=require("compression");
+const fileUpload=require("express-fileupload");
+const helmet=require("helmet");
+
+dotenv.config();
+
+const app=express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(fileUpload());
+
+const port=process.env.port || 7878;
+
+app.listen(port,()=>{
+  console.log("Running");
+});
+
